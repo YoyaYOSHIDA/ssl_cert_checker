b1ddf399b3ad7baab4caa60527124e1fc56c1243
YoyaYOSHIDA <yoyayehoshua@gmail.com>
2021-02-16 20:22:40 +0900

    Init

---
+++ README.md
@@ -0,0 +1,51 @@
+# [ssl_cert_checker](https://github.com/YoyaYOSHIDA/ssl_cert_checker)
+
+Checks validity and lifetimes of SSL certificates of your web servers.
+
+Based on: [do_not_forget_the_expire_date_of_ssl_ certificate_of_your_web_server.md](https://gist.github.com/YoyaYOSHIDA/91e432bd447d2f074af321d7160044e5/edit)
+
+# How to use
+
+## List URLs
+
+List URLs of web servers in [clients.list](./clients.list) .
+
+Example:
+
+```
+myserver1.com
+myserver2.net
+myserver3.co.uk
+```
+
+## Run script
+
+Simply execute for Linux:
+
+```bash
+./ssl_cert_checker_linux.sh
+```
+
+Or for MacOS:
+
+```
+./ssl_cert_checker_macos.sh
+```
+
+# Output
+
+The output will be for example:
+
+```
+myserver1.com,Valid,20220303
+myserver2.net,Expired,20210101
+myserver3.co.uk,Unavailable,00000000
+```
+
+The output syntax is `<URL>,<Valid | Expired | Unavailable>,<Last date of certificate lifetime>` .
+
+Note that you don't need to consider whether HTTPS is available or not (in case of HTTPS is unavailable, the output will return `Unavailable`). 
+
+# Appendix
+
+You can use this script by modifying a little and running on a regular basis by cron and send the result to Slack if the expire date is comming not to forget the expire date of an SSL certificate.
---
+++ ssl_cert_checker_linux.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#title           : ssl_cert_checker_linux.sh
+#description     : Checks validity and lifetimes of SSL certificates of your web servers.
+#arg             : -
+#author          : YoyaYOSHIDA(https://github.com/YoyaYOSHIDA)
+#usage           : Execute manually.
+#notes           : For Linux.
+#==============================================================================
+
+# script dir
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+# dates
+DATE_TODAY_UNIX=$(date +%s)
+
+for CLIENT_URL in $(cat ${SCRIPT_DIR}/clients.list); do
+  if openssl s_client -connect ${CLIENT_URL}:443 < /dev/null 2> /dev/null | openssl x509 -text -noout -enddate > /dev/null 2>&1; then
+    PRE_RESULT="$(openssl s_client -connect ${CLIENT_URL}:443 < /dev/null 2> /dev/null | openssl x509 -text -noout -enddate | grep "Not After")"
+    PRE_VALID_UNTIL="$(echo ${PRE_RESULT} | sed -E "s/^Not\sAfter\s+:\s+//g" | awk '{print $1" "$2" "$4}')"
+    VALID_UNTIL="$(date -d "${PRE_VALID_UNTIL}" '+%Y%m%d')"
+    VALID_UNTIL_UNIX="$(date --date "${VALID_UNTIL} 00:00" +%s)"
+    
+    if [ ${DATE_TODAY_UNIX} -lt ${VALID_UNTIL_UNIX} ]; then
+      echo "${CLIENT_URL},Valid,${VALID_UNTIL}"
+    else
+      echo "${CLIENT_URL},Expired,${VALID_UNTIL}"
+    fi
+  else
+    echo "${CLIENT_URL},Unavailable,00000000"
+  fi
+done
+
